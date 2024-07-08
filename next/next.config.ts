3644f6b00209083460e086d75c33641a2907dd6d
yechan <kevin2695@naver.com>
2024-07-08 22:20:30 +0900

    [next] next.config.ts 설정

---
+++ next/next.config.ts
@@ -0,0 +1,11 @@
+/**@type {import('next').nextConfig} */
+
+const nextConfig = {
+  reactStrictMode: true,
+  poweredByHeader: false,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+};
+
+module.exports = nextConfig;
